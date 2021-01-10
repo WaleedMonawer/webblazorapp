aa2510f49c8c6906b3b511d4962ae7954fd8927d
WaleedMonawer <wmonawer.se@gmail.com>
2021-01-10 19:28:58 +0300

    Add files via upload

---
+++ js/myCode.js
@@ -0,0 +1,7 @@
+﻿
+function showhideLoading(showhide) {
+
+    if (showhide == true)
+        document.getElementById("loadingimag").style.display = "block";
+    else document.getElementById("loadingimag").style.display = "none";
+}
